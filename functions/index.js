cba57de6f6a23d941fcc37b81a0fb5c7afe139dd
algg17 <93863221+algg17@users.noreply.github.com>
2025-08-05 23:23:24 +0500

    Add files via upload

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,5 @@
 const functions = require("firebase-functions");
-const logger = require("firebase-functions/logger");
 
 exports.api = functions.https.onRequest((req, res) => {
-  logger.info("Request received");
-  res.send("Hello from Firebase v1!");
+  res.send("Hello from Firebase!");
 });
-
