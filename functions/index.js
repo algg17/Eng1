b3a4aea9f668ee8b3a2f7895409e696764c8adb0
algg17 <93863221+algg17@users.noreply.github.com>
2025-08-05 21:00:26 +0500

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,8 @@
-const { onRequest } = require("firebase-functions");
+const functions = require("firebase-functions");
 const logger = require("firebase-functions/logger");
 
 exports.api = functions.https.onRequest((req, res) => {
+  logger.info("Request received");
   res.send("Hello from Firebase v1!");
 });
+
