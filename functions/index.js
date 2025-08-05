b94e4b44b7263d1549b6ae3d62ecc151fe808533
algg17 <93863221+algg17@users.noreply.github.com>
2025-08-05 20:55:45 +0500

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,6 @@
 const { onRequest } = require("firebase-functions");
 const logger = require("firebase-functions/logger");
 
-exports.api = onRequest((req, res) => {
-  logger.info("Incoming request", { method: req.method, path: req.path });
-
-  if (req.path === "/" || req.path === "/home") {
-    res.send("<h1>Hello from Firebase Functions!</h1>");
-  } else if (req.path === "/ping") {
-    res.json({ pong: true });
-  } else {
-    res.status(404).send("Not found");
-  }
+exports.api = functions.https.onRequest((req, res) => {
+  res.send("Hello from Firebase v1!");
 });
